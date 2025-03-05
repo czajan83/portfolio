65b7d6b1fbc9f4e5d2896cfcb560d49aadec8365
andrzej <andrzej.b.czajka@gmail.com>
2025-03-05 20:30:39 +0100

    output data stored in jenkins folder

--- playwright/tests/jobListPortal2.spec.ts
+++ playwright/tests/jobListPortal2.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 import { JobsList } from "../POM/jobListPortal2POM";
 
 var fs = require("fs");
-var dir_portal = "../jobPortal2/";
+var dir_portal = "jobPortal2/";
 var dir_jobname = dir_portal + "testerOprogramowania/"
 var dir_details = dir_jobname + "details/"
 var jobs = [{link: '', requirements: '', fileName: ""}];
